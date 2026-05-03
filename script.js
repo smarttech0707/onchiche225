9c5e98839617cc60f8d1da78795b00b0ce02cabd
Onchiche <onchiche225@gmail.com>
2026-05-03 16:56:16 +0000

    Mise à jour numéro WhatsApp

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /** Numéro WhatsApp (indicatif 225 + numéro sans le 0 initial, chiffres uniquement). */
-const WHATSAPP_NUMBER = "225501487570";
+const WHATSAPP_NUMBER = "225701678342";
 window.WHATSAPP_NUMBER = WHATSAPP_NUMBER;
 
 /** E-mail utilisé par le formulaire contact (mailto). */
